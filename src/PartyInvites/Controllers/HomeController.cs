e95e605caf801d254724a0f8a2f3029cacfab9e7
k3kc <k3kc@DESKTOP-3JVSJL9>
2017-04-05 10:35:43 +0500

    Add project files.

---
+++ src/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponse()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+    }
+}
